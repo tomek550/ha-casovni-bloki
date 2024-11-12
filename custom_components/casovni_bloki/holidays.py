36f479258ab5706970cda88295306e22fce33755
tomek550 <tomi.ivencnik@gmail.com>
2024-11-12 08:48:01 +0100

    Create holidays.py

---
+++ custom_components/casovni_bloki/holidays.py
@@ -0,0 +1,32 @@
+HOLIDAYS = [
+    # Holidays for 2024
+    "2024-01-01",  # New Year's Day
+    "2024-01-02",  # Second January
+    "2024-02-08",  # February 8th
+    "2024-04-01",  # April 1st
+    "2024-05-01",  # May 1st
+    "2024-05-02",  # May 2nd
+    "2024-06-25",  # June 25th
+    "2024-08-15",  # August 15th
+    "2024-10-31",  # October 31st
+    "2024-11-01",  # November 1st
+    "2024-12-25",  # Christmas Day
+    "2024-12-26",  # December 26th
+
+    # Holidays for 2025
+    "2025-01-01",  # New Year's Day
+    "2025-01-02",  # Second January
+    "2025-02-08",  # February 8th
+    "2025-04-20",  # April 20th
+    "2025-04-21",  # April 21st
+    "2025-04-27",  # April 27th
+    "2025-05-01",  # May 1st
+    "2025-05-02",  # May 2nd
+    "2025-06-08",  # June 8th
+    "2025-06-25",  # June 25th
+    "2025-08-15",  # August 15th
+    "2025-10-31",  # October 31st
+    "2025-11-01",  # November 1st
+    "2025-12-25",  # Christmas Day
+    "2025-12-26"   # December 26th
+]
